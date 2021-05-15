f0479c521db0e6c8d927a7eaaa408e05c0d8c68b
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-05-15 19:22:46 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-git clone "https://gitee.com/blueveryday/lede.git"
+git clone "https://github.com/coolsnowwolf/lede.git"
+
+sed -i 's/192.168.1.1/192.168.62.1/g' package/base-files/files/bin/config_generate
+
+sed -i '343s/4/8/g' target/linux/ar71xx/image/tiny-tp-link.mk
+
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
+
 pushd "lede"; ./scripts/feeds update -a -f; ./scripts/feeds install -a -f; popd
