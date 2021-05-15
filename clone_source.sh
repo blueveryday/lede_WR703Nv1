a5c4fc7664adffe088296e9403012b18530c17e6
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-05-15 20:48:10 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -2,19 +2,13 @@
 
 git clone "https://github.com/coolsnowwolf/lede.git"
 
-cd /home/runner/lede
+pushd "lede"; 
 
-sed -i 's/192.168.1.1/192.168.62.1/g' package/base-files/files/bin/config_generate
-
-sed -i '343s/4/8/g' target/linux/ar71xx/image/tiny-tp-link.mk
-
-#sed -i 's/#//g' feeds.conf.default
-
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
-
-#pushd "lede"; 
+sed -i 's/192.168.1.1/192.168.62.1/g' package/base-files/files/bin/config_generate        #修改路由器管理IP地址
+sed -i '343s/4/8/g' target/linux/ar71xx/image/tiny-tp-link.mk                             #修改wr703nv1的编译固件大小
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
 
 ./scripts/feeds update -a -f
 ./scripts/feeds install -a -f
 
-#; popd
+popd
