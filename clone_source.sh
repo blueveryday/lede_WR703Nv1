fdf7bb64e3ece994fa47299feae912e57b26c44c
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-05-15 18:01:22 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git clone "https://gitee.com/blueveryday/lede.git"
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 pushd "lede"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
